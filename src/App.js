b0c854a3f6596e1602395c4fc0433e0f38168654
[JyothisShajan] <[jyothisshajan41@gmail.com]>
2021-09-13 10:38:51 +0530

    updated menu-items with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Switch , Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage.component';
+
+const HatsPAge =()=>(
+  <div>
+  <h1>Hats PAge</h1>
+  </div>
+)
+
+
+
+
 function App() {
   return (
-    <div >
-     <Homepage/>
-    </div>
+    <Switch>
+    <Route exact path='/' component={Homepage}/>
+     <Route exact path='/hats' component={HatsPAge}/>
+    </Switch>
+
   );
 }
 
